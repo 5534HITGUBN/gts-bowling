4956df266182b77c8bd645b774753b6d0e65d616
mark.sweitzer@marksweitzer.io <msweitzer1987@gmail.com>
2020-02-07 14:59:20 -0500

    build 5

--- src/components/Frame/Frame.js
+++ src/components/Frame/Frame.js
@@ -5,9 +5,9 @@ import FrameScore from './FrameScore/FrameScore';
 
 const Frame = (props) => {
     return (
-        <div className={props.currentFrame+1 === props.frameNumber ? 'Frame':'Frame Frame--hidden-mobile' }>
+        <div className={(props.currentFrame+1 === props.frameNumber )|| (props.currentFrame === props.frameNumber )? 'Frame':'Frame Frame--hidden-mobile' }>
             <div className="Frame__frame-number">{`Frame ${props.frameNumber}`}</div>
-            <div className={props.currentFrame+1 === props.frameNumber ? 'Frame__frame-indicator  Frame__frame-indicator--active' : 'Frame__frame-indicator' }></div>
+            <div className={props.currentFrame+1 === props.frameNumber? 'Frame__frame-indicator  Frame__frame-indicator--active' : 'Frame__frame-indicator' }></div>
             <div className="Frame__content">
                 <Rolls frameData={props.frameData} lastFrame={props.lastFrame}/> 
                 <FrameScore frameData={props.frameData}/>
